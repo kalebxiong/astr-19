6c80c1bd20cad886622602a8df070b351e54cc6d
kalebxiong <146883084+kalebxiong@users.noreply.github.com>
2023-10-19 15:58:11 -0700

    Session 1 prompt for coding journal 1

---
+++ session1.py
@@ -0,0 +1,12 @@
+#name
+print("Kaleb Xiong",)
+
+#movie
+print("Favorite movie is Cars",)
+print("Fact 1: I have watched Cars since I was 4",)
+print("Fact 2: I had Lightning McQueen blankets as a kid",)
+
+#food
+print("Favorite food is pizza",)
+print("Fact 1: I love Sals pizza in my hometown",)
+print("Fact 2: My favorite type is chicken bacon ranch pizza",)
