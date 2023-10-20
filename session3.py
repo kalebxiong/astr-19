6adb3bf5da4a16622481a415759e50cac6f94623
kalebxiong <146883084+kalebxiong@users.noreply.github.com>
2023-10-20 10:35:08 -0700

    session 3 prompt for coding journal 1

---
+++ session3.py
@@ -0,0 +1,14 @@
+# Define the function f(x)
+def f(x):
+    return x**3 + 8
+
+# Call f(x) with x = 9
+x = 9
+result = f(x)
+
+# Print the result
+print("Result of f(x) with x =", x, "is:", result)
+
+# Check if the result is larger than 27 and print "YAY!" if it is
+if result > 27:
+    print("YAY!")
