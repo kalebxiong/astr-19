7228eec6ea2a2ce0da50b4e78aca4be395e2a35a
kalebxiong <146883084+kalebxiong@users.noreply.github.com>
2023-10-19 17:03:44 -0700

    Session 2 prompt for journal 1

---
+++ session2.py
@@ -0,0 +1,20 @@
+# Define two floating point numbers and two integers
+float_num1 = 7.5
+float_num2 = 3.2
+int_num1 = 10
+int_num2 = 4
+
+# Calculate the sum of two floating point numbers
+sum_result = float_num1 + float_num2
+print("Sum Result:", sum_result)
+print("Data Type of Sum Result:", type(sum_result))
+
+# Calculate the difference between two integers
+difference_result = int_num1 - int_num2
+print("Difference Result:", difference_result)
+print("Data Type of Difference Result:", type(difference_result))
+
+# Calculate the product of a floating point number and an integer
+product_result = float_num1 * int_num2
+print("Product Result:", product_result)
+print("Data Type of Product Result:", type(product_result))
